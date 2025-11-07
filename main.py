ce4cf6f22fd465f75f373cfc03bee395a35697b5
ludomontanaro17-bit <ludomontanaro17@gmail.com>
2025-11-07 10:44:02 +0100

    project inizialization

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+step 1: caricamento del dataset
+step 2: analisi esplorativa del dataset (inizializzo la classe di preprocessing, mostro informazioni generali e caratteristiche del dataset, visualizzo distribuzioni grafiche, matrice di correlazione, noto eventuali valori mancanti)
+step 3: data preprocessing (rimuovo le variabili collineari, tratto i valori mancanti, standardizzazione o normalizzazione e aggiorno il dataframe 'modificato', suddivisione testing e training set)
+step 4: creazione e addestramento dei modelli, previsioni sul validation set, report di classificazione, salvataggio del modello nel percorso corretto
+'''
+
